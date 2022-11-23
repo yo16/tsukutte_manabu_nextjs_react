a7448a62a4d92c767768c4ee9499206797f3152e
yo16 <xxyo1xx@gmail.com>
2022-11-23 22:57:12 +0900

    absolute path

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,11 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import React from 'react'
-import styles from '../styles/Home.module.css'
-
-import Header from '../components/header'
-import Hero from '../components/hero'
-import Footer from '../components/footer'
+import Header from 'components/header'
+import Hero from 'components/hero'
+import Footer from 'components/footer'
 
 export default function Home() {
   return (
