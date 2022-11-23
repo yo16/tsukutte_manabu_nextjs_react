6841f216b8bc51533ecb7fd9e8ac1c174db3c5cd
yo16 <xxyo1xx@gmail.com>
2022-11-23 23:06:14 +0900

    edit Custom-App component

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -2,11 +2,5 @@ import Layout from 'components/layout'
 import Hero from 'components/hero'
 
 export default function Home() {
-  return (
-    <>
-      <Layout>
-        <Hero />
-      </Layout>
-    </>
-  )
+  return <Hero />
 }
