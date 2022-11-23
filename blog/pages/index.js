a2b8d829dd253a692463031ec674bc4cb4f1ed71
yo16 <xxyo1xx@gmail.com>
2022-11-23 23:01:04 +0900

    add Layout.js

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,17 +1,12 @@
-import Header from 'components/header'
+import Layout from 'components/layout'
 import Hero from 'components/hero'
-import Footer from 'components/footer'
 
 export default function Home() {
   return (
     <>
-      <Header />
-
-      <main>
+      <Layout>
         <Hero />
-      </main>
-
-      <Footer />
+      </Layout>
     </>
   )
 }
