bc128471667c74ea84a49bd25e9ece94ebb808f8
yo16 <xxyo1xx@gmail.com>
2022-11-23 22:53:58 +0900

    page components

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -3,37 +3,20 @@ import Image from 'next/image'
 import React from 'react'
 import styles from '../styles/Home.module.css'
 
-function Decoration(props) {
-  return <div style={{ color: 'red' }}>{props.children}</div>
-}
-
-function EchoPost(props) {
-  return (
-    <article>
-      <Decoration>
-        <h3>{props.title}</h3>
-        <hr />
-      </Decoration>
-    </article>
-  )
-}
+import Header from '../components/header'
+import Hero from '../components/hero'
+import Footer from '../components/footer'
 
 export default function Home() {
-  const subtitle = 'サブタイトルあああ'
-
   return (
     <>
-      <header style={{ backgroundColor: '#c33' }}>HEADER!</header>
+      <Header />
 
       <main>
-        <h1 style={{ color: 'red', fontSize: '80px' }}>CUBE*a</h1>
-        <hr />
-        <p>{subtitle}</p>
-        <EchoPost title="タイトル１" />
-        <EchoPost title="タイトル２" />
+        <Hero />
       </main>
 
-      <footer style={{ backgroundColor: '#3c3' }}>FOOTER!!</footer>
+      <Footer />
     </>
   )
 }
