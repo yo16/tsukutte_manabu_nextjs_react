cf944f70c287e72d5554c33c05dd7af75b204639
yo16 <xxyo1xx@gmail.com>
2022-11-23 22:29:31 +0900

    component

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -3,20 +3,30 @@ import Image from 'next/image'
 import React from 'react'
 import styles from '../styles/Home.module.css'
 
+function EchoPost() {
+  return (
+    <article>
+      <h3>記事のタイトル</h3>
+    </article>
+  )
+}
+
 export default function Home() {
   const subtitle = 'サブタイトルあああ'
 
   return (
     <>
-      <header>HEADER!</header>
+      <header style={{ backgroundColor: '#c33' }}>HEADER!</header>
 
       <main>
         <h1 style={{ color: 'red', fontSize: '80px' }}>CUBE*a</h1>
         <hr />
         <p>{subtitle}</p>
+        <EchoPost />
+        <EchoPost />
       </main>
 
-      <footer>FOOTER!!</footer>
+      <footer style={{ backgroundColor: '#3c3' }}>FOOTER!!</footer>
     </>
   )
 }
