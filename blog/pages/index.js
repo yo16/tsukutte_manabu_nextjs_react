dbe9f533cc7e154db1205e1ecc02929ceee6203b
yo16 <xxyo1xx@gmail.com>
2022-11-23 22:19:32 +0900

    JSXのおさほう

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,7 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import React from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return <h1>CUBE*</h1>
+  const subtitle = 'サブタイトルあああ'
+
+  return (
+    <>
+      <h1 style={{ color: 'red', fontSize: '80px' }}>CUBE*a</h1>
+      <hr />
+      <p>{subtitle}</p>
+    </>
+  )
 }
