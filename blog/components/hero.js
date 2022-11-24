8e045217aa81035d39bb958664cb4ece1908b828
yo16 <xxyo1xx@gmail.com>
2022-11-24 23:05:15 +0900

    typo

--- blog/components/hero.js
+++ blog/components/hero.js
@@ -2,7 +2,7 @@ import styles from 'styles/hero.module.css'
 
 export default function Hero({ title, subtitle, imageOn = false }) {
   return (
-    <div classNmae={styles.text}>
+    <div className={styles.text}>
       <h1 className={styles.title}>{title}</h1>
       <p className={styles.subtitle}>{subtitle}</p>
       {imageOn && <figure>[画像]</figure>}
