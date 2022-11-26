123703a2c4f553439efd5492f623132e93a4bb36
yo16 <xxyo1xx@gmail.com>
2022-11-26 23:36:59 +0900

    header style

--- blog/components/header.js
+++ blog/components/header.js
@@ -1,9 +1,10 @@
 import Logo from 'components/logo'
 import Nav from 'components/nav'
+import styles from 'styles/header.module.css'
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.flexContainer}>
       <Logo boxOn />
       <Nav />
     </header>
