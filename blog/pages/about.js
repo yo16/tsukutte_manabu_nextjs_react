8f6877f46b3d8916da05b5cd7fd995d4b3693e7b
yo16 <xxyo1xx@gmail.com>
2022-11-27 00:45:28 +0900

    set width other page

--- blog/pages/about.js
+++ blog/pages/about.js
@@ -1,5 +1,13 @@
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function About() {
-  return <Hero title="About" subtitle="だいたい" />
+  return (
+    <Container>
+      <Hero title="About" subtitle="だいたい" />
+      <p>てすと段落</p>
+      <h2>テストのヘッダー２</h2>
+      <p>段落その次</p>
+    </Container>
+  )
 }
--- blog/pages/index.js
+++ blog/pages/index.js
@@ -1,6 +1,10 @@
-import Layout from 'components/layout'
+import Container from 'components/container'
 import Hero from 'components/hero'
 
 export default function Home() {
-  return <Hero title="CUBE" subtitle="アウトプットしていくサイトー" imageOn />
+  return (
+    <Container>
+      <Hero title="CUBE" subtitle="アウトプットしていくサイトー" imageOn />
+    </Container>
+  )
 }
