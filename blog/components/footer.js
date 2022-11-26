7205da83346fe2899e2e05f435da19bc4aa889b3
yo16 <xxyo1xx@gmail.com>
2022-11-27 00:09:24 +0900

    style footer

--- blog/components/footer.js
+++ blog/components/footer.js
@@ -1,12 +1,13 @@
 import Logo from 'components/logo'
+import styles from 'styles/footer.module.css'
 
 export default function Footer() {
   return (
-    <footer>
-      <hr />
-      --- footer !! ---
-      <br />
-      <Logo />
+    <footer className={styles.wrapper}>
+      <div className={styles.flexContainer}>
+        <Logo />
+        [ソーシャル]
+      </div>
     </footer>
   )
 }
