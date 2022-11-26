aaf927c588952943c8c452dcdb434ebd59425738
yo16 <xxyo1xx@gmail.com>
2022-11-27 01:05:03 +0900

    add post-body

--- blog/pages/about.js
+++ blog/pages/about.js
@@ -1,13 +1,17 @@
 import Container from 'components/container'
 import Hero from 'components/hero'
+import PostBody from 'components/post-body'
 
 export default function About() {
   return (
     <Container>
       <Hero title="About" subtitle="だいたい" />
-      <p>てすと段落</p>
-      <h2>テストのヘッダー２</h2>
-      <p>段落その次</p>
+
+      <PostBody>
+        <p>てすと段落</p>
+        <h2>テストのヘッダー２</h2>
+        <p>段落その次</p>
+      </PostBody>
     </Container>
   )
 }
