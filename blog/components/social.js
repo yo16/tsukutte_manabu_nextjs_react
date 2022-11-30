5a07f8dbf5b4e3731cdd607bca2e47ad327fb355
yo16 <xxyo1xx@gmail.com>
2022-11-30 23:52:40 +0900

    add social components

---
+++ blog/components/social.js
@@ -0,0 +1,33 @@
+import styles from 'styles/social.module.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faTwitter,
+  faFacebookF,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons'
+
+export default function Social() {
+  return (
+    <ul className={styles.list}>
+      <li>
+        <a href="https://twitter.com/">
+          <fontAwesomeIcon icon={faTwitter} />
+          <span className="sr-only">Twitter</span>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.facebook.com/">
+          <fontAwesomeIcon icon={faFacebookF} />
+          <span className="sr-only">Facebook</span>
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/">
+          <fontAwesomeIcon icon={faGithub} />
+          <span className="sr-only">GitHub</span>
+        </a>
+      </li>
+    </ul>
+  )
+}
