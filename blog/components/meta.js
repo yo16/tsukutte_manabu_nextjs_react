4c2fcbfd90bc042eb0843c9454e5d4c689c60a4c
yo16 <yo16@CHARDONNAY.localdomain>
2022-12-01 23:41:57 +0900

    add favicon

--- blog/components/meta.js
+++ blog/components/meta.js
@@ -26,6 +26,13 @@ export default function Meta({ pageTitle, pageDesc }){
 
             <link rel="canonical" href={url} />
             <meta property="og:url" content={url} />
+
+            <meta property="og:site_name" content={siteTitle} />
+            <meta property="og:type" content={siteType} />
+            <meta property="og:local" content={siteLocale} />
+
+            <link rel="icon" href={siteIcon} />
+            <link rel="apple-touch-icon" href={siteIcon} />
         </Head>
     )
 }
