eb5d9c4d3d872f9ad30fd15fc77f246cd7ac6f98
yo16 <yo16@CHARDONNAY.localdomain>
2022-12-01 22:50:41 +0900

    add Meta

--- blog/pages/about.js
+++ blog/pages/about.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import PostBody from 'components/post-body'
@@ -14,9 +14,7 @@ import eyecatch from 'images/about.jpg'
 export default function About() {
   return (
     <Container>
-      <Head>
-        <title>アバウト</title>
-      </Head>
+      <Meta pageTitle="あぼうｔ" />
 
       <Hero title="About" subtitle="だいたい" />
 
