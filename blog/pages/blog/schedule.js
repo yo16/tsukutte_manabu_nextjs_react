540351c281a6d000ac99750190200eb70966106e
yo16 <xxyo1xx@gmail.com>
2022-12-03 13:56:45 +0900

    addgetPostBySlug

--- blog/pages/blog/schedule.js
+++ blog/pages/blog/schedule.js
@@ -1,22 +1,32 @@
-import { client } from 'lib/api'
+import { getPostBySlug } from 'lib/api'
+import Container from 'components/container'
 
-export default function Schedule() {
-  return <h1>記事のタイトル</h1>
+export default function Schedule({
+  title,
+  publish,
+  content,
+  eyecatch,
+  categories,
+}) {
+  return (
+    <Container>
+      <h1>{title}</h1>
+    </Container>
+  )
 }
 
 export async function getStaticProps() {
-  const resPromise = client.get({
-    endpoint: 'blogs',
-  })
+  const slug = 'schedule'
 
-  try {
-    const res = await resPromise
-    console.log(res)
-  } catch (err) {
-    console.log(err)
-  }
+  const post = await getPostBySlug(slug)
 
   return {
-    props: {},
+    props: {
+      title: post.title,
+      publish: post.publishDate,
+      content: post.content,
+      eyecatch: post.eyecatch,
+      categories: post.categories,
+    },
   }
 }
